a2bb830eac4e240ebb9e12de1e47817cf7256a4a
Your Name <your.email@example.com>
2026-02-06 19:27:42 +0530

    fetch,axios and keys topics of  day3 notes committed

---
+++ React/Day3/index.js
@@ -0,0 +1,23 @@
+let obj = {
+  name: "Pratik Raj",
+  role: "developer",
+  city: "Patna",
+};
+
+let data = Object.entries(obj);
+console.log(data);
+
+let value = Object.values(obj);
+console.log(value);
+
+let keys = Object.keys(obj);
+console.log(keys);
+
+//Q. Take an object as of above one and use method Object.entries() to print all the key value pairs and
+// you have to access the first array and print the value from it.
+
+// [
+//   ["name", "Pratik Raj"],
+//   ["role", "developer"],
+//   ["city", "Patna"],
+// ];//
